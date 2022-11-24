0c59d4ac7f1796f74e9b875493cb04bfd4641e9f
Usuario 1 <usuario1@192.168.1.110>
2022-11-23 20:58:10 -0500

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hello';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 9;
+const nuevoNum = 1234;
 
 // Crea una variable booleana:
 const nuevoBool = null;
